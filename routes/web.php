c9a1bb13e93ff50e09416e57db29bad30583e7b6
Humdiess <hmzinchannel@gmail.com>
2026-05-09 07:30:40 +0700

    fix (auth) : fixing auth routing

--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,21 @@ Route::get('/berita/{post:slug}', [NewsController::class, 'show'])->name('news.s
 Route::get('/kategori/{category:slug}', [NewsController::class, 'category'])->name('news.category');
 Route::get('/search', [NewsController::class, 'search'])->name('news.search');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-
-Route::get('/dashboard/posts', [PostController::class, 'index'])->name('dashboard.posts.index');
-
-Route::get('/dashboard/posts/create', [PostController::class, 'create'])->name('dashboard.posts.create');
-
-Route::get('/dashboard/categories', [CategoryController::class, 'index'])->name('dashboard.categories.index');
-
-Route::get('/dashboard/users', [UserController::class, 'index'])->name('dashboard.users.index');
-
 Route::middleware('auth')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard/posts', [PostController::class, 'index'])->name('dashboard.posts.index');
+    Route::get('/dashboard/posts/create', [PostController::class, 'create'])->name('dashboard.posts.create');
+    Route::get('/dashboard/categories', [CategoryController::class, 'index'])->name('dashboard.categories.index');
+    Route::get('/dashboard/users', [UserController::class, 'index'])->name('dashboard.users.index');
+
+    // Resource CRUD
     Route::resource('posts', PostController::class);
     Route::resource('categories', CategoryController::class);
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::resource('users', UserController::class);
-    Route::get('/users', [UserController::class, 'index'])
-            ->name('users.index');
-
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])
-            ->name('users.destroy');
-
 });
 
 require __DIR__ . '/auth.php';
